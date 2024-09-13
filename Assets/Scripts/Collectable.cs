ec8b530b9c97c7523eea5428a8e6afc74cf46094
sladeel <163821768+sladeel@users.noreply.github.com>
2024-09-13 11:27:01 +1000

    updated collection script
    collectables can now be dragged out of the screen. if this happens they vanish into the void(?) never to be seen again. unsure the cause of this.

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -9,8 +9,12 @@ public class Collectable : MonoBehaviour
     public GameObject mouse;
     public Collider2D notebook;
     Vector2 position;
+    public float minX = -5.243f;
+    public float maxX = 5.253f;
+    public float minY = -3.723f;
+    public float maxY = 3.755f;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,9 +32,16 @@ public class Collectable : MonoBehaviour
         {
             Debug.Log("Success! Hit " + hit.collider);
         }
-        if ((/*mouse.GetComponent<Collider2D>().IsTouching(text) ||*/ (hit.collider == text)) && Input.GetMouseButton(0))
+        if ((mouse.GetComponent<Collider2D>().IsTouching(text) || (hit.collider == text)) && Input.GetMouseButton(0))
         {
-            transform.position = mouse.transform.position;
+            if (transform.position.x >= minX && transform.position.x <= maxX && transform.position.y >= minY && transform.position.y <= maxY)
+            {
+                transform.position = mouse.transform.position;
+            }
+            else
+            {
+                transform.position = Input.mousePosition;
+            }
             if (notebook.IsTouching(text))
             {
                 //add to notebook array
