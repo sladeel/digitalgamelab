2812aecc6b8a58925d4382694f0271dc307ca3f2
sladeel <163821768+sladeel@users.noreply.github.com>
2024-09-20 20:18:02 +1000

    added desk zoom

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public string position;
+    public Camera cam;
+    private float velocityZoom;
+    private Vector3 velocityMove;
+    public float smoothTime = 0.3f;
+    public float maxSpeed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(position)
+        {
+            case "screen":
+                cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, 5, ref velocityZoom, smoothTime, maxSpeed);
+                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(0, 0, -10), ref velocityMove, smoothTime, maxSpeed);
+                break;
+            case "desk":
+                cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, 10, ref velocityZoom, smoothTime, maxSpeed);
+                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(0, -1.15f, -10), ref velocityMove, smoothTime, maxSpeed);
+                break;
+        }
+    }
+}
