0feb3861eccad31dd90bbf8f847d8791605aecd8
sladeel <163821768+sladeel@users.noreply.github.com>
2024-09-20 21:38:07 +1000

    added zoom functionality

---
+++ Assets/Scripts/ZoomIn.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomIn : MonoBehaviour
+{
+    
+    public CameraController cam;
+    public bool requireClick = false;
+    bool click;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LayerMask mask = LayerMask.GetMask("Zoom In");
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 1, mask);
+        
+        if (requireClick == true)
+        {
+            click = Input.GetMouseButtonDown(0);
+        }
+        else
+        {
+            click = true;
+        }
+
+        if (hit.collider != null && click)
+        {
+            cam.position = "screen";
+            Debug.Log(hit.collider);
+        }
+    }
+}
---
+++ Assets/Scripts/ZoomOut.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOut : MonoBehaviour
+{
+    public CameraController cam;
+    public bool requireClick = false;
+    bool click;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LayerMask mask = LayerMask.GetMask("Zoom Out");
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 1, mask);
+
+        if (requireClick == true)
+        {
+            click = Input.GetMouseButtonDown(0);
+        }
+        else
+        {
+            click = true;
+        }
+
+        if (hit.collider != null)
+        {
+            cam.position = "desk";
+            Debug.Log(hit.collider);
+        }
+
+
+    }
+}
