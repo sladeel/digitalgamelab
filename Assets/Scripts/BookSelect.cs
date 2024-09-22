f9ac0a63a004e6e0dd18c6027e2e85465d1f54aa
sladeel <163821768+sladeel@users.noreply.github.com>
2024-09-22 16:04:41 +1000

    added ability to pull up notebook

---
+++ Assets/Scripts/BookSelect.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookSelect : MonoBehaviour
+{
+    public CameraController cam;
+    public NotebookController book;
+    public bool requireClick = false;
+    bool click;
+    public SpriteRenderer visualBook;
+    public Collider2D collide;
+    public Collider2D mouse;
+
+    public Collider2D leftCollide;
+    public Collider2D rightCollide;
+    public Collider2D screenCollide;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LayerMask mask = LayerMask.GetMask("Book");
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 1, mask);
+
+        if (requireClick == true)
+        {
+            click = Input.GetMouseButtonDown(0);
+        }
+        else
+        {
+            click = true;
+        }
+
+        if (hit.collider != null && book.position != "book")
+        {
+            visualBook.color = new Color(1, 0.38f, 1, 0.17f);
+        }
+        else if (book.position != "book")
+        {
+            visualBook.color = new Color(1, 0.38f, 1, 0);
+        }
+        else
+        {
+            visualBook.color = new Color(1, 0.38f, 1, 0.17f);
+        }
+
+        if (hit.collider != null && click)
+        {
+            cam.position = "desk";
+            book.position = "book";
+            leftCollide.enabled = !leftCollide.enabled;
+            rightCollide.enabled = !rightCollide.enabled;
+            screenCollide.enabled = !screenCollide.enabled;
+            Debug.Log(hit.collider);
+        }
+    }
+}
