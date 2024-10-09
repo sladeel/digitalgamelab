5fb00fcbf6e48ba19fe63d326b79ef221e781df5
sladeel <163821768+sladeel@users.noreply.github.com>
2024-10-09 17:51:56 +1100

    quashed bugs

--- Assets/Scripts/NoteHandler.cs
+++ Assets/Scripts/NoteHandler.cs
@@ -8,7 +8,7 @@ public class NoteHandler : MonoBehaviour
     Vector2 difference;
     Collider2D clue;
     Renderer clueRender;
-    bool onPage = false;
+    public bool onPage = false;
     public GameObject notebook;
     Collider2D notebookCollider;
     public NotebookHandler notebookHandler;
@@ -35,21 +35,24 @@ public class NoteHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LayerMask mask = LayerMask.GetMask("Collectable");
+        LayerMask mask = LayerMask.GetMask("Clue");
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 1, mask);
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         
         startPosition = parentClue.transform.position;
 
-        /*if (hit.collider == text)
+        if (hit.collider == clue && Input.GetMouseButton(0))
         {
             Debug.Log("Success! Hit " + hit.collider);
-        }*/
+        }
         if (/*(mouse.GetComponent<Collider2D>().IsTouching(clue) ||*/ (hit.collider == clue) && Input.GetMouseButton(0))
         {
-            difference = mousePosition - new Vector2 (transform.position.x, transform.position.y);
+            //difference = mousePosition - new Vector2 (transform.position.x, transform.position.y);
+            difference = new Vector2(0, 0);
 
-            transform.position = Vector2.SmoothDamp(transform.position, (mousePosition - difference), ref currentVelocity, smoothTime, maxMoveSpeed);
+            //transform.position = Vector2.SmoothDamp(transform.position, (mousePosition - difference), ref currentVelocity, smoothTime, maxMoveSpeed);
+            transform.position = mousePosition;
+
 
             if (notebookCollider.IsTouching(clue))
             {
