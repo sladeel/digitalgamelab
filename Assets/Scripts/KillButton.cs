bae908be0d3ad4280cff4a2f21e24f3ee32b0c53
sladeel <163821768+sladeel@users.noreply.github.com>
2024-10-21 00:59:09 +1100

    added closing cutscene

---
+++ Assets/Scripts/KillButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillButton : MonoBehaviour
+{
+    public string scene;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LayerMask mask = LayerMask.GetMask("Hover");
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 1, mask);
+
+        if (hit.collider != null && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
