c1c8a3a334fcbd126c296f45194a1a824f0b8149
sladeel <163821768+sladeel@users.noreply.github.com>
2024-10-23 21:24:46 +1100

    improved load times

--- Assets/Scripts/PreloadScene.cs
+++ Assets/Scripts/PreloadScene.cs
@@ -1,24 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Diagnostics;
+using UnityEngine.SceneManagement;
 
-public class QuitGame : MonoBehaviour
+public class PreloadScene : MonoBehaviour
 {
+    public string scene;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AsyncOperation preloader = SceneManager.LoadSceneAsync(scene);
+        preloader.allowSceneActivation = false;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void Quit()
-    {
-        Application.Quit();
     }
 }
